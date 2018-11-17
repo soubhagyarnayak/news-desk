8a3c79ff52ee46dc09ce17c78d609f8a9b926e19
Soubhagya Nayak <soubhagya.r.nayak@gmail.com>
2018-11-17 10:00:27 +0530

    Adding pg module and getting data from postgre

--- router/main.js
+++ router/main.js
@@ -1,11 +1,26 @@
 'use strict'
 
+var pg = require("pg");
+
+var connectionString = "postgres://postgres:<pwd>@localhost:5432/feeds";
+const pool = new pg.Pool({
+    connectionString: connectionString,
+  });
+
 module.exports = function(app)
 {
     app.get('/',function(req,res){
         res.send('News Reader');
 	});
     app.get('/hn',function(req,res){
-        res.render('hackernews.html')
+        //res.send('hackernews.html');
+        pool.query('SELECT * FROM hackernewsarticles order by CreateTime DESC', function(err,result) {
+            pool.end(); // closing the connection;
+            if(err){
+                console.log(err);
+                res.status(400).send(err);
+            }
+            res.status(200).send(result.rows);
+        });
     });
 }
