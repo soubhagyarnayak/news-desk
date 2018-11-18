be742b320273741edf82c73761ca073d06f32d58
Soubhagya Nayak <soubhagya.r.nayak@gmail.com>
2018-11-18 07:59:26 +0530

    added dependency on body-parser to parse json requests

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 'use strict'
 
-var express    =    require('express');
-var app        =    express();
+var express = require("express");
+var app = express();
 
 require('./router/main')(app);
 app.set('view engine', 'ejs');
 app.set('views',__dirname + '/views');
 
-var server     =    app.listen(3000,function(){
+var server =  app.listen(3000,function(){
     console.log("news reader running on port 3000.");
 });
