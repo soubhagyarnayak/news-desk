3a500eabbaf55df59ca20b1ac79308e0f2cf08b1
Soubhagya Nayak <soubhagya.r.nayak@gmail.com>
2018-11-25 20:19:43 +0530

    updated the bootstrap css link to use locally installed package

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ require('./router/main')(app);
 app.set('view engine', 'ejs');
 app.set('views',__dirname + '/views');
 app.use(express.static(__dirname + '/public'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 app.listen(3000,'0.0.0.0',function(){
     console.log("news reader running on port 3000.");
